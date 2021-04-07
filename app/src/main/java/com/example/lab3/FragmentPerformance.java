6388dac8cf0b4578638a2198c4b4b43143981690
imaginebaraka <barakamulumia@gmail.com>
2021-04-07 12:57:47 +0300

    Toast performance implemented

--- app/src/main/java/com/example/lab3/FragmentPerformance.java
+++ app/src/main/java/com/example/lab3/FragmentPerformance.java
@@ -4,15 +4,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class FragmentPerformance extends Fragment {
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_performance, container, false);
+        View view =  inflater.inflate(R.layout.fragment_performance, container, false);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if(userProfile != null){
+                    String html = userProfile.html;
+                    String  css = userProfile.css;
+                    String javascript = userProfile.javascript;
+
+                    Toast.makeText(getContext(), html, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/lab3/User.java
+++ app/src/main/java/com/example/lab3/User.java
@@ -1,7 +1,7 @@
 package com.example.lab3;
 
 public class User {
-    public String fullName, age, email;
+    public String fullName, age, email, html, css, javascript;
 
     public User(){
 
@@ -11,5 +11,14 @@ public class User {
         this.fullName = fullName;
         this.age = age;
         this.email = email;
+    }
+
+    public User(String fullName, String age, String email, String html, String css, String javascript){
+        this.fullName = fullName;
+        this.age = age;
+        this.email = email;
+        this.html = html;
+        this.css = css;
+        this.javascript = javascript;
     }
 }
